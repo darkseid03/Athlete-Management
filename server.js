116fce816022e5765fac0aeff8d80fcb872e2bb5
darkseid03 <riteshroshanmohanty123@gmail.com>
2025-04-07 16:25:37 +0000

    Updated deployment

--- server.js
+++ server.js
@@ -5,14 +5,34 @@ const PORT = 3001;
 
 app.use(express.static(path.join(__dirname, 'fitness')));
 
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fitness', 'html', 'index.html'));
+});
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'fitness', 'html', 'home.html'));
+    res.sendFile(path.join(__dirname, 'fitness', 'html', 'home.html'))
 });
 
-app.get('/', (req, res) => {
-  res.redirect('/fitness/html/index.html');
+app.get('/bmi_calculator.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fitness', 'html', 'bmi_calculator.html'));
+});
+
+app.get('/diet_planner.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fitness', 'html', 'diet_planner.html'));
+});
+
+app.get('/progress_tracker.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fitness', 'html', 'progress_tracker.html'));
+});
+
+app.get('/home.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fitness', 'html', 'home.html'));
+
+});
+app.get('/training_plan.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'fitness', 'html', 'training_plan.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
